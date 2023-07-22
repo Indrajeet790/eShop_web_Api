c682834d442bb43b95f7f4c38f5b66b5200a525f
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-07-22 13:04:42 +0530

    Initial setup

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const PORT = 4000;
+
+// middleware
+app.use(bodyParser.json());
+app.use(morgan("tiny"));
+
+// required env
+require("dotenv/config");
+app.get(`${api}/products`, (req, res) => {
+  const newProduct = {};
+});
+
+const api = process.env.API_URL;
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log("error to run server");
+  } else {
+    console.log(`server is running on port ${PORT}`);
+  }
+});
