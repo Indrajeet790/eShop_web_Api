339d8e69c312d5fc210e71a9ba3d5cd5f450ac40
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-07-22 15:46:34 +0530

    write code for product controlller

---
+++ controllers/product.js
@@ -0,0 +1,10 @@
+const Product = require("../models/product");
+
+module.exports.addProduct = async (req, res) => {
+  try {
+    const newProduct = await Product.create(req.body);
+    res.status(201).json(newProduct);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
