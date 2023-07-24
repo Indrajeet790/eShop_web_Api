8211f1f1b319dae213ebcdcd60bc3b5beff70298
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-07-24 17:33:16 +0530

    all category routes

---
+++ routes/category.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const categoryController = require("../controllers/Category");
+const router = express.Router();
+
+// find all category  categoryList
+router.get("/categoryList", categoryController.categoryList);
+
+// find category with the help of id
+router.get("/categoryList/:id", categoryController.findCategory);
+
+// create a new category
+router.post("/createCategory", categoryController.addCategory);
+
+// delete a category with the help of id
+router.delete("/delete/:id", categoryController.deleteCategory);
+
+module.exports = router;
