3b57054fd949f4084508b086065c4db75e2eecd3
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-07-24 22:02:14 +0530

    find all product list if poroduct is present

--- controllers/product.js
+++ controllers/product.js
@@ -1,5 +1,22 @@
 const Product = require("../models/product");
 
+module.exports.productList = async (req, res) => {
+  try {
+    // find productList if product present
+    const productList = await Product.find({});
+    if (!productList) {
+      return res
+        .status(404)
+        .json({ success: false, message: "product is not found" });
+      // res.status(500).json({ success: false });
+    } else {
+      res.status(200).send(productList);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 module.exports.addProduct = async (req, res) => {
   try {
     const newProduct = await Product.create(req.body);
