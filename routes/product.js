bb2266e59f172b75518d7b61d0aa88758c4b9dcf
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-07-24 22:26:07 +0530

    create api for product

--- routes/product.js
+++ routes/product.js
@@ -2,6 +2,19 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/product");
 
-router.post("/add", productController.addProduct);
+// find all product  productList
+router.get("/list", productController.productList);
+
+// find product with the help of id
+router.get("/list/:id", productController.productById);
+
+// create a new product
+router.post("/create", productController.addProduct);
+
+// delete a product with the help of id
+router.delete("/delete/:id", productController.deleteProduct);
+
+// update a product with the help of id
+router.put("/update/:id", productController.updateProduct);
 
 module.exports = router;
